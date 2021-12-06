41a01189cd75d47598e84cac12a99e97b105582a
sg130 <gowth130@gmail.com>
2021-12-06 23:20:46 +0100

    Added RNN code (not debugged), SPL not ready

--- src/stsgcn/models.py
+++ src/stsgcn/models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from stsgcn.layers import ST_GCNN_layer, CNN_layer
+from stsgcn.layers import ST_GCNN_layer, CNN_layer, SPL
 
 
 class ZeroVelocity(nn.Module):
@@ -109,3 +109,94 @@ class STSGCN(nn.Module):
             x = self.prelus[i](self.txcnns[i](x)) + x  # residual connection
         x = x.permute(0, 1, 3, 2)  # (NTCV->NTVC)
         return x
+
+class RNNSPL(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.model_name = "rnn_spl"
+        self.recurrent_cell = cfg["recurrent_cell"]
+        self.use_spl = cfg["use_spl"]
+        self.dense_spl = cfg["dense_spl"]
+        
+        self.train_input_time_frame = cfg["input_n"] + cfg["output_n"]
+        self.train_output_time_frame = cfg["input_n"] + cfg["output_n"]
+        self.test_input_time_frame = cfg["input_n"]
+        self.test_output_time_frame = cfg["output_n"]
+        
+        self.input_channels = cfg["input_dim"]
+        self.output_channels = cfg["output_dim"]
+        self.output_joints = cfg["output_joints"]
+        self.input_dropout_rate = cfg["input_dropout_rate"]
+        self.input_hidden_size = cfg["input_hidden_size"]
+        self.cell_size = cfg["cell_size"]
+        self.output_hidden_size = cfg["output_hidden_size"]
+        self.residual_velocity = cfg["residual_velocity"]
+        
+        self.input_dropout = nn.Dropout(p=self.input_dropout_rate)
+        self.input_layer = nn.Linear(self.input_channels, self.input_hidden_size)
+        
+        if self.recurrent_cell == "lstm":
+            self.rnn = nn.LSTMCell(self.input_hidden_size,self.cell_size)
+        elif self.recurrent_cell == 'gru':
+            self.rnn = nn.GRUCell(self.input_hidden_size,self.cell_size)
+        elif self.recurrent_cell == 'rnn':
+            self.rnn = nn.RNNCell(self.input_hidden_size,self.cell_size)
+        else:
+            raise Exception("Unknown Recurrent Cell module")
+        
+        if self.use_spl:
+            self.output_layer = SPL(input_features = self.cell_size,
+                                    hidden_features = self.output_hidden_size,
+                                    output_joints=self.output_joints,
+                                    dense=self.dense_spl)
+        else:
+            self.output_layer = nn.Sequential([nn.Linear(self.cell_size,self.output_hidden_size), nn.ReLU(), nn.Linear(self.output_hidden_size, self.output_channels)])
+        
+    def forward(self, x):
+        """ 
+            Parameters:
+            input (torch.tensor): Input motion sequence with shape (N, T_in, V, C)
+
+            Returns:
+            torch.tensor: Output motion sequence with shape (N, T_out, V, C)
+
+            N: number of sequences in the batch
+            in_channels: 3D joint coordinates or axis angle representations
+            out_channels: 3D joint coordinates or axis angle representations
+            T_in: number of frames in the input sequence
+            T_out: number of frames in the output sequence
+            V: number of joints
+        """
+        x = x.view(x.shape[0], x.shape[1], self.input_dim)
+        x = self.input_layer(self.input_dropout(x))
+        
+        if self.training:
+            cur_x = x[:,0,:].squeeze(1)
+            h = self.rnn(cur_x)
+            y = [self.output_layer(h) + cur_x]
+            
+            for i in range(x.shape[1]-1):
+                cur_x = x[:,i+1,:].squeeze(1)
+                h = self.rnn(cur_x, h)
+                y.append(self.output_layer(h) + cur_x)
+            
+            y = y.view(x.shape[0], x.shape[1], -1, 3)
+        
+        else:
+            cur_x = x[:,0,:].squeeze(1)
+            h = self.rnn(cur_x)
+            for i in range(x.shape[1]-1):
+                cur_x = x[:,i+1,:].squeeze(1)
+                h = self.rnn(cur_x, h)
+                
+            cur_x = x[:,-1,:].squeeze(1)
+            all_y = []
+            for i in range(self.test_output_time_frame):
+                cur_x = y
+                h = self.rnn(cur_x, h)
+                y = self.output_layer(h) + cur_x
+                all_y.append(y)
+            
+            y = all_y.view(x.shape[0], self.test_output_time_frame, -1, 3)
+        return y
