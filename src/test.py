28bd6730fcd24f2e45e5baa43fe140430635b2f8
Gowtham Senthil <gsenthi@eu-login-10.euler.ethz.ch>
2022-01-12 18:37:39 +0100

    STSEncoder + GRU Cells

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 from stsgcn.test import test
+config_path = "../configs/test_rnn_stsE.yaml"
 #config_path = "../configs/test_rnn.yaml"
-config_path = "../configs/test_stsgcn.yaml"
+#config_path = "../configs/test_stsgcn.yaml"
 #config_path = "../configs/test_zero.yaml"
 test(config_path)
